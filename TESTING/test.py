65b570fc39b56039988db84632d48b4ac17ec5a8
Bernardino Sassoli <sassoli@gmail.com>
2020-12-21 16:02:17 +0100

    Added charting functionality in 'TESTING' directory

--- TESTING/test.py
+++ TESTING/test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, Response, json
 from axelrod import Lattice
 # Configure application
 app = Flask(__name__)
@@ -18,25 +18,35 @@ def after_request(response):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    model = Lattice(10, 3, 3)
-    model.initialize()
-    iters = 1000000
-    steps = []
-    data = []
-    for step in range(iters):
-        model.update()
-        if (iters - step) % 10000 == 0:
-            final_cultures = (len(model.get_culture_count()),
-                              model.get_culture_count())
-            steps.append(step)
-            data.append(final_cultures[0])
-            if model.equilibrium() is True:
-                print("Equilibrium")
-                break
-            else:
-                print(
-                    f"Step: {step}. Current cultures: {data}")
-    return render_template("index.html", steps=steps, cultures=data)
+    return render_template("index.html")
+
+
+@app.route('/chart-data')
+def chart_data():
+    def generate_chart_data():
+        while True:
+            model = Lattice(40, 4, 12)
+            model.initialize()
+            iters = 100000
+            steps = []
+            data = []
+            for step in range(iters):
+                model.update()
+                if (iters - step) % 10000 == 0:
+                    final_cultures = (len(model.get_culture_count()),
+                                      model.get_culture_count())
+                    steps.append(step)
+                    data.append(final_cultures[0])
+                    if model.equilibrium() is True:
+                        print("Equilibrium")
+                        break
+                    else:
+                        print(
+                            f"Step: {step}. Current cultures: {data}")
+                    json_data = json.dumps(
+                        {'time': step, 'value': final_cultures[0]})
+                    yield f"data:{json_data}\n\n"
+    return Response(generate_chart_data(), mimetype='text/event-stream')
 
 
 if __name__ == '__main__':
